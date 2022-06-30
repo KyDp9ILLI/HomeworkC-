8788544310049196b7598df4022d19900a824cfc
KyDp9ILLI <a-chikilev@mail.ru>
2022-06-30 23:45:34 +0300

    add task 23

---
+++ 30.06.2022/task_23/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+// int a = 1;
+// while (a<=n)
+// {
+//     Console.WriteLine(Math.Pow(a,3) + " ");
+//     a++;
+// }
+Console.WriteLine("Таблица кубов");
+
+for (int i = 1; i<=n; i++)
+{
+    Console.WriteLine($"{i} в кубе = {i*i*i} ");
+}
