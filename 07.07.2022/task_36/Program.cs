c9fc07d49f8334a34abd2afb9f1c9d7ff9c9c6e3
KyDp9ILLI <a-chikilev@mail.ru>
2022-07-12 15:19:36 +0300

    seminar 5

---
+++ 07.07.2022/task_36/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+Console.Write("Введите размер массива:");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное значение элементов массива:");
+int minimal = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное значение элементов массива:");
+int maximal = Convert.ToInt32(Console.ReadLine());
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0)
+            Console.Write("[");
+        if (i < arr.Length - 1)
+            Console.Write(arr[i] + ",");
+        else
+            Console.Write(arr[i] + "]");
+    }
+    Console.WriteLine();
+}
+
+int SumOddIndex (int[] arr)
+{
+    int sumNum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i % 2 >0)
+        {
+            sumNum += arr[i];
+        }
+    }
+    return sumNum;
+ 
+}
+
+int[] array = CreateArrayRndInt(sizeArr, minimal, maximal);
+//PrintArray(array);
+int summa = SumOddIndex(array);
+Console.WriteLine("Сумма элементов, стоящих на нечетных позициях = " + summa);
---
+++ 07.07.2022/task_38/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+
+Console.Write("Введите размер массива:");
+int sizeArr = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите минимальное значение элементов массива:");
+int minimal = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное значение элементов массива:");
+int maximal = Convert.ToInt32(Console.ReadLine());
+
+double[] CreateArrayRndInt(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Math.Round(rnd.Next(min, max + 1) + rnd.NextDouble(),1);
+    }
+    return arr;
+}
+
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0)
+            Console.Write("[");
+        if (i < arr.Length - 1)
+            Console.Write(arr[i] + ";");
+        else
+            Console.Write(arr[i] + "]");
+    }
+    Console.WriteLine();
+}
+
+double DiffMaxMinElem(double[] arr)
+{
+    double MinElem = arr[0];
+    double MaxElem = arr[0];
+
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < MinElem)
+            MinElem = arr[i];
+        if (arr[i] > MaxElem)
+            MaxElem = arr[i];
+    }
+    return MaxElem - MinElem;
+}
+
+double[] array = CreateArrayRndInt(sizeArr, minimal, maximal);
+PrintArray(array);
+double diff = DiffMaxMinElem(array);
+Console.WriteLine($"Разница между максимальным и минимальным элементом массива = {diff}");
